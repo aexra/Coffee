59c9c5ac08f02ee0611a91391acea81aa730946d
Никита Фомин <defdXs@yandex.ru>
2024-04-19 22:36:30 +0300

    Added EntityTypeConfigurations for Users and Meetings

---
+++ backend/Coffee/Coffee.Persistence/EntityTypeConfigurations/MeetingConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Coffee.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Coffee.Persistence.EntityTypeConfigurations;
+
+public class MeetingConfiguration : IEntityTypeConfiguration<Meeting>
+{
+    public void Configure(EntityTypeBuilder<Meeting> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.HasIndex(x => x.Id).IsUnique();
+    }
+}
