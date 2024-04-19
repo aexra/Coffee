eca09274eeb39e7f90db20a916c66a78f3a53daa
Никита Фомин <defdXs@yandex.ru>
2024-04-19 22:58:15 +0300

    Persistence almost done

---
+++ backend/Coffee/Coffee.Persistence/CoffeeDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Coffee.Application.Interfaces;
+using Coffee.Domain.Models;
+using Coffee.Persistence.EntityTypeConfigurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coffee.Persistence;
+public sealed class CoffeeDbContext : DbContext, ICoffeeDbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Meeting> Coming { get; set; }
+    public DbSet<Meeting> Success { get; set; }
+    public DbSet<Meeting> Failed { get; set; }
+
+    public CoffeeDbContext(DbContextOptions<CoffeeDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new UserConfiguration());
+        builder.ApplyConfiguration(new MeetingConfiguration());
+        base.OnModelCreating(builder);
+    }
+}
---
+++ backend/Coffee/Coffee.Persistence/DbInitializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffee.Persistence;
+public class DbInitializer
+{
+    public static void Initialize(CoffeeDbContext context)
+    {
+        context.Database.EnsureCreated();
+    }
+}
