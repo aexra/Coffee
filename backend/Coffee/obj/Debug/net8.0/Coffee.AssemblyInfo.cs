644bd129e216907e9e4e2f021f1fef8763b7ad6d
Никита Фомин <defdXs@yandex.ru>
2024-04-19 20:10:28 +0300

    Updated .gitignore

--- backend/Coffee/obj/Debug/net8.0/Coffee.AssemblyInfo.cs
+++ backend/Coffee/obj/Debug/net8.0/Coffee.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coffee")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bdd4904165570584380c86490a947f1badb713b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51ee29f81f17c75a8ed5d35ba64c87447d44dd60")]
 [assembly: System.Reflection.AssemblyProductAttribute("Coffee")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coffee")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
