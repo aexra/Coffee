f1700691f14dd66e82beb0ed24daf5591fba0553
Никита Фомин <defdXs@yandex.ru>
2024-04-20 20:32:54 +0300

    User model formatting xd

--- backend/Coffee/Models/User.cs
+++ backend/Coffee/Models/User.cs
@@ -4,16 +4,16 @@ namespace Coffee.Models;
 
 public class User : IdentityUser
 {
-    public DateOnly? HiredSince { get; set; }
-    public Image? Avatar { get; set; }
-    public uint MeetingsCount { get; set; }
-    public string? Name { get; set; }
-    public string? Surname { get; set; }
-    public string? Patronymic { get; set; }
-    public string? Position { get; set; }
-    public string? Hobbies { get; set; }
-    public string? Pets { get; set; }
-    public string? Coffee { get; set; }
-    public string? Telegram { get; set; }
-    public string? Vk { get; set; }
+    public DateOnly?    HiredSince      { get; set; }
+    public Image?       Avatar          { get; set; }
+    public uint         MeetingsCount   { get; set; }
+    public string?      Name            { get; set; }
+    public string?      Surname         { get; set; }
+    public string?      Patronymic      { get; set; }
+    public string?      Position        { get; set; }
+    public string?      Hobbies         { get; set; }
+    public string?      Pets            { get; set; }
+    public string?      Coffee          { get; set; }
+    public string?      Telegram        { get; set; }
+    public string?      Vk              { get; set; }
 }
