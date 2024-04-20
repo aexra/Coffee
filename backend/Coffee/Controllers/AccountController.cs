e4352036b924761140621d4f2bddcbdf437936c3
Никита Фомин <defdXs@yandex.ru>
2024-04-21 01:00:35 +0300

    Removed useless usings

--- backend/Coffee/Controllers/AccountController.cs
+++ backend/Coffee/Controllers/AccountController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using static SQLite.SQLite3;
 
 namespace Coffee.Controllers;
 
