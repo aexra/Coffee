4c4da1e6a61d09b7ac19ae217c592f16bf58bd30
Никита Фомин <defdXs@yandex.ru>
2024-04-20 11:27:27 +0300

    Created database context class

---
+++ backend/Coffee/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Coffee.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coffee.Data;
+
+public class DataContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Image> Images { get; set; }
+    public DbSet<Theme> Themes { get; set; }
+    public DbSet<FutureMeeting> FutureMeetings { get; set; }
+    public DbSet<CompletedMeeting> CompletedMeetings { get; set; }
+
+    public string DbPath { get; }
+
+    public DataContext()
+    {
+        DbPath = "Database/LocalDB.db";
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite($"Data Source={DbPath}");
+}
