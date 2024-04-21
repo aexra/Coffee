1974287439dc02f9bab8079d793fed778078e60c
Никита Фомин <defdXs@yandex.ru>
2024-04-21 03:42:38 +0300

    Added condition handler to switch to reg page

--- frontend/js/formSwapper.js
+++ frontend/js/formSwapper.js
@@ -6,11 +6,20 @@ const loginForm = document.getElementById("login-f");
 const regForm = document.getElementById("reg-f");
 
 regBtn.addEventListener("click", (e) => {
+    if (!regConditionHandler()) return;
+
     baseForm.style.display = "none";
     loginForm.style.display = "none";
     regForm.style.display = "flex";
 });
 
 loginBtn.addEventListener("click", (e) => {
-
+    baseForm.style.display = "none";
+    regForm.style.display = "none";
+    loginForm.style.display = "flex";
 });
+
+// Здесь нужно прописать условие перехода на фрейм регистрации
+function regConditionHandler() {
+    return true;
+}
