8afacf166d3fcd20abc3d2b3989ced98c6c066bf
Никита Фомин <defdXs@yandex.ru>
2024-04-21 04:00:23 +0300

    Some sort of register page

--- frontend/js/formSwapper.js
+++ frontend/js/formSwapper.js
@@ -5,21 +5,56 @@ const baseForm = document.getElementById("base-f");
 const loginForm = document.getElementById("login-f");
 const regForm = document.getElementById("reg-f");
 
+const applyRegisterBtn = document.getElementById("applyRegisterBtn");
+const closeRegFormBtn = document.getElementById("closeRegFormBtn");
+
 regBtn.addEventListener("click", (e) => {
     if (!regConditionHandler()) return;
 
-    baseForm.style.display = "none";
-    loginForm.style.display = "none";
-    regForm.style.display = "flex";
+    switchFrame(1);
 });
 
 loginBtn.addEventListener("click", (e) => {
-    baseForm.style.display = "none";
-    regForm.style.display = "none";
-    loginForm.style.display = "flex";
+    switchFrame(2);
+});
+
+applyRegisterBtn.addEventListener("click", (e) => {
+
+});
+
+closeRegFormBtn.addEventListener("click", (e) => {
+    switchFrame(0);
 });
 
 // Здесь нужно прописать условие перехода на фрейм регистрации
 function regConditionHandler() {
     return true;
+}
+
+// Здесь нужно прописать логику регистрации
+function registerNewUser() {
+
+}
+
+
+function switchFrame(i) {
+    switch (i) {
+        case 0:
+            baseForm.style.display = "flex";
+            regForm.style.display = "none";
+            loginForm.style.display = "none";
+            break;
+        case 1:
+            baseForm.style.display = "none";
+            regForm.style.display = "flex";
+            loginForm.style.display = "none";
+            break;
+        case 2:
+            baseForm.style.display = "none";
+            regForm.style.display = "none";
+            loginForm.style.display = "flex";
+            break;
+        default:
+            break;
+    }
 }
